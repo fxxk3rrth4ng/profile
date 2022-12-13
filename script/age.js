e08a7ab0e306ea667ca3bc207b4b513d06d637fb
fxxk3rrth4ng <baech00@kakao.com>
2022-12-13 16:33:17 +0000

    ^q^

--- script/age.js
+++
@@ -1,9 +0,0 @@
-var dday = new Date("January 1, 2003, 0:00:00").getTime();
-
-setInterval(function () {
-	var today = new Date().getTime();
-	var gap = today - dday;
-	var age = Math.floor(gap / 3153600000) / 10 + 1;
-
-	document.getElementById("count").innerHTML = age + "세 (2003년생)";
-}, 1000);
